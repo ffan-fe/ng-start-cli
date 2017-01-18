de7ce4682b9e8ceb120b312615aedd192c85b408
guanbingchang <guanbingchang@wanda.cn>
2017-01-18 17:26:27 +0800

    update add fancyui

--- template/client/app/app.js
+++ template/client/app/app.js
@@ -10,16 +10,13 @@ import angular from 'angular';
 import Common from './common/common';
 import Components from './components/components';
 import AppComponent from './app.component';
-import 'angular-busy/dist/angular-busy';
-import 'angular-busy/angular-busy.css';
 import router from './router/router';
 import './skin/theme.less';
 
 angular.module('app', [
     router.name,
     Common.name,
-    Components.name,
-    'cgBusy'
+    Components.name
   ])
   .constant('$menuConstant', {
     DEBUG: process.env.DEBUG
